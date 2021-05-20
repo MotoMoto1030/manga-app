7a0cd21fd60db5b87def4d22c7b45cae91d202bc
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-20 18:45:21 +0900

    Active_Storageの導入

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,15 @@
 class ComicsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+  private
+
+  def comic_params
+    params.require(:comic).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
