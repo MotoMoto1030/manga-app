f4c7a9bb6d718710fc4a21758436336e4684c104
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-21 12:46:00 +0900

    Active_Strageの導入(修正1)

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -10,6 +10,6 @@ class ComicsController < ApplicationController
   private
 
   def comic_params
-    params.require(:comic).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:comic).permit(:nickname, :image, :birthday).merge(user_id: current_user.id)
   end
 end
