8969d5c975e49e4de503dc74e9c3333a7ee7fbe2
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-24 13:39:59 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'comics#index'
-  resources :comics, only: [:index, :new, :create]
   resources :users, only: [:show]
+  resources :comics, only: [:index, :new, :create] do
+    resources :likes, only: [:create, :destroy]
+  end
+
 end
