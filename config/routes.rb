a52b6743dc6fe3f4a167649611e1ff2a648b38c5
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-24 14:10:46 +0900

    コントローラーアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'comics#index'
   resources :users, only: [:show]
-  resources :comics, only: [:index, :new, :create] do
+  resources :comics, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create, :destroy]
   end
 
